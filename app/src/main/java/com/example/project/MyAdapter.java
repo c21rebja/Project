ee3ca8fa4b62983096a7f3feb1f953ab14a9f197
c21rebja <c21rebja@student.his.se>
2022-05-16 15:49:15 +0200

    added myadapter class

--- app/src/main/java/com/example/project/MyAdapter.java
+++ app/src/main/java/com/example/project/MyAdapter.java
@@ -1,5 +1,35 @@
 package com.example.project;
 
-public class MyAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    private List<Berries> berryList;
+    public MyAdapter(List<Berries> berryList) {
+        this.berryList = berryList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder (@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.name.setText(berryList.get(position).getName());
+        holder.type.setText(berryList.get(position).getType());
+    }
+
+    @Override
+    public int getItemCount() {
+        return berryList.size();
+    }
 }
