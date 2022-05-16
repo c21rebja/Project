f31e951655c0cec5ae305ef0560ff161ce7db81e
c21rebja <c21rebja@student.his.se>
2022-05-16 15:54:06 +0200

    filled my activity with things from the networking assignment to create a working recycler view

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     RecyclerView recyclerView;
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=c21rebja";
+    public List<Berries> berryList;
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recycler_view);
+        adapter = new MyAdapter(berryList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+
+
     }
 }
